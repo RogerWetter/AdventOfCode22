a68b8e9c409d52da341ff25b4162c52c05c1c298
RogerWetter <79643516+RogerWetter@users.noreply.github.com>
2022-12-01 09:34:28 +0100

    day 1

---
+++ src/2/script.js
@@ -0,0 +1,15 @@
+import {readFileSync} from 'fs';
+
+
+let split = readFileSync('input.txt', 'utf-8').split(/\r?\n/)
+let counter = 0
+let res = "Hello"
+split.forEach((value) => {
+  if (value) {
+    //todo
+  } else {
+    //todo
+  }
+})
+console.log(res)
+console.log(res)
